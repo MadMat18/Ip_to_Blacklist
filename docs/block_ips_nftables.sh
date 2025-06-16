b77299f739cf1cab0115d7e24e014fa4c886e2b3
root <6moricejules6@gmail.com>
2025-06-16 23:59:01 +0200

    Auto update 2025-06-16 23:59:01

--- docs/block_ips_nftables.sh
+++ docs/block_ips_nftables.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Script nftables auto-généré
 
+nft add rule inet filter input ip saddr 190.97.227.171 drop
 nft add rule inet filter input ip saddr 10.42.228.104 drop
+nft add rule inet filter input ip saddr 105.113.72.132 drop
+nft add rule inet filter input ip saddr 45.6.173.94 drop
+nft add rule inet filter input ip saddr 39.39.52.118 drop
+nft add rule inet filter input ip saddr 1.160.235.39 drop
+nft add rule inet filter input ip saddr 57.129.64.10 drop
 nft add rule inet filter input ip saddr 34.95.113.255 drop
-nft add rule inet filter input ip saddr 210.91.73.167 drop
-nft add rule inet filter input ip saddr 185.91.127.81 drop
-nft add rule inet filter input ip saddr 199.232.214.172 drop
-nft add rule inet filter input ip saddr 207.167.67.206 drop
-nft add rule inet filter input ip saddr 10.42.228.106 drop
 nft add rule inet filter input ip saddr 160.44.201.156 drop
-nft add rule inet filter input ip saddr 86.54.31.44 drop
-nft add rule inet filter input ip saddr 34.107.221.82 drop
+nft add rule inet filter input ip saddr 185.91.127.81 drop
