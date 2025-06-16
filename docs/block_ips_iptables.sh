2e67c899b80c58bb0ea4b61817c71944d2286376
root <6moricejules6@gmail.com>
2025-06-16 11:32:01 +0200

    Ajout des scripts et de la page HTML

---
+++ docs/block_ips_iptables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script iptables auto-généré
+
+iptables -A INPUT -s 10.42.228.104 -j DROP
+iptables -A INPUT -s 34.95.113.255 -j DROP
+iptables -A INPUT -s 210.91.73.167 -j DROP
+iptables -A INPUT -s 185.91.127.81 -j DROP
+iptables -A INPUT -s 199.232.214.172 -j DROP
+iptables -A INPUT -s 207.167.67.206 -j DROP
+iptables -A INPUT -s 10.42.228.106 -j DROP
+iptables -A INPUT -s 160.44.201.156 -j DROP
+iptables -A INPUT -s 86.54.31.44 -j DROP
+iptables -A INPUT -s 34.107.221.82 -j DROP
---
+++ docs/block_ips_nftables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script nftables auto-généré
+
+nft add rule inet filter input ip saddr 10.42.228.104 drop
+nft add rule inet filter input ip saddr 34.95.113.255 drop
+nft add rule inet filter input ip saddr 210.91.73.167 drop
+nft add rule inet filter input ip saddr 185.91.127.81 drop
+nft add rule inet filter input ip saddr 199.232.214.172 drop
+nft add rule inet filter input ip saddr 207.167.67.206 drop
+nft add rule inet filter input ip saddr 10.42.228.106 drop
+nft add rule inet filter input ip saddr 160.44.201.156 drop
+nft add rule inet filter input ip saddr 86.54.31.44 drop
+nft add rule inet filter input ip saddr 34.107.221.82 drop
