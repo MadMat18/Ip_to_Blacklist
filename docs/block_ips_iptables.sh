e991581f45b33291c3e55a90f4a28f36a968be70
root <6moricejules6@gmail.com>
2025-06-18 13:55:54 +0200

    Auto update 2025-06-18 13:55:54

--- docs/block_ips_iptables.sh
+++ docs/block_ips_iptables.sh
@@ -3,7 +3,6 @@
 
 iptables -A INPUT -s 59.95.79.172 -j DROP
 iptables -A INPUT -s 14.173.137.103 -j DROP
-iptables -A INPUT -s 172.27.0.2 -j DROP
 iptables -A INPUT -s 180.252.227.107 -j DROP
 iptables -A INPUT -s 185.246.128.133 -j DROP
 iptables -A INPUT -s 116.98.172.118 -j DROP
@@ -11,3 +10,4 @@ iptables -A INPUT -s 41.90.42.28 -j DROP
 iptables -A INPUT -s 106.51.151.72 -j DROP
 iptables -A INPUT -s 116.105.214.155 -j DROP
 iptables -A INPUT -s 193.105.134.95 -j DROP
+iptables -A INPUT -s 147.45.179.82 -j DROP
--- docs/block_ips_nftables.sh
+++ docs/block_ips_nftables.sh
@@ -3,7 +3,6 @@
 
 nft add rule inet filter input ip saddr 59.95.79.172 drop
 nft add rule inet filter input ip saddr 14.173.137.103 drop
-nft add rule inet filter input ip saddr 172.27.0.2 drop
 nft add rule inet filter input ip saddr 180.252.227.107 drop
 nft add rule inet filter input ip saddr 185.246.128.133 drop
 nft add rule inet filter input ip saddr 116.98.172.118 drop
@@ -11,3 +10,4 @@ nft add rule inet filter input ip saddr 41.90.42.28 drop
 nft add rule inet filter input ip saddr 106.51.151.72 drop
 nft add rule inet filter input ip saddr 116.105.214.155 drop
 nft add rule inet filter input ip saddr 193.105.134.95 drop
+nft add rule inet filter input ip saddr 147.45.179.82 drop
